3ed656e49686c4a5b7976cb5954bcd0c2a7c8da4
young2good <ufo2989@gmail.com>
2024-10-18 11:26:26 +0900

    gg

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -19,7 +19,7 @@ with st.sidebar:
     st.header('Snowflake Healthcare App')
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     my_cur = my_cnx.cursor()
-    run a snowflake query and put it all in a var called my_catalog
+    # run a snowflake query and put it all in a var called my_catalog
     my_cur.execute("select * from SWEATSUITS")
     my_catalog = my_cur.fetchall()
     st.dataframe(my_catalog)
